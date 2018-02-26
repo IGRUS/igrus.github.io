346b736d4dd776f55785ae47f15dc2f7ea168792
Jo Tae Sang <gsts007@gmail.com>
2018-02-26 17:02:50 +0900

    Add introduce page

--- js/layout.js
+++ js/layout.js
@@ -4,24 +4,52 @@
         return this.charAt(0).toUpperCase() + this.substring(1).toLowerCase()
     };
 
-    // Loading
-    (function () {
-        var section = location.hash.substring(1);
-        if (!section) return;
-
-        document.title = 'IGRUS - ' + section.toUpperCaseFirst();
-    })();
-
     // On Dom Ready
     $(function () {
-
-        // Menu Click Events
         (function () {
-            var $scrollTarget = $('html, body');
             var $nav = $('header nav');
+            var $menus = $nav.find('a');
+            var $sections = $('body>section');
+            var $scrollTarget = $('html,body');
+            var $button = $nav.find('button');
+            var $menuWrap = $nav.find('.menu-wrap');
             var MIN_TIME = 300;
 
-            $nav.find('>a').on('click', function (e) {
+            function highlightMenu() {
+                var middle = $(window).scrollTop() + $(window).innerHeight() / 2;
+
+                var $target = null;
+                var distance = Number.MAX_VALUE;
+
+                $.each($sections, function () {
+                    var $section = $(this);
+                    var dist = Math.abs($section.offset().top + $section.height() / 2 - middle);
+
+                    if (distance > dist) {
+                        $target = $section;
+                        distance = dist;
+                    }
+                });
+
+                var name = $target.data('section');
+                $menus.removeClass('active');
+                $nav.find('a[data-target=' + name + ']').addClass('active');
+            }
+
+            $nav.on('focusin', function (e) {
+                e.stopPropagation();
+                $menuWrap.addClass('xs sm');
+            });
+
+            $(document).on('focusin', function () {
+                $menuWrap.removeClass('xs sm');
+            });
+
+            // Highlight Menu
+            $(window).on('scroll', highlightMenu);
+            highlightMenu();
+
+            $menus.on('click', function (e) {
                 e.preventDefault();
 
                 var section = $(this).attr('href').substring(1);
@@ -32,11 +60,7 @@
                 var top = $section.offset().top;
                 var diff = Math.abs(pastTop - top);
 
-                $scrollTarget.animate({ scrollTop: top }, Math.min(diff, MIN_TIME), 'swing');
-
-                var title = 'IGRUS - ' + section.toUpperCaseFirst();
-                history.pushState({ section: section }, title, '#' + section);
-                document.title = title;
+                $scrollTarget.stop().animate({ scrollTop: top }, Math.min(diff, MIN_TIME), 'swing');
 
                 return false;
             });
@@ -44,7 +68,8 @@
 
         // Banner
         (function () {
-            var $banner = $('section.main .banner');
+            var $main = $('section[data-section=main]');
+            var $banner = $main.find('.banner');
             var $items = $banner.find('.items');
             var $wrapper = $items.find('.items-wrapper');
 
@@ -54,8 +79,8 @@
             var now = 0;
 
             function move(index) {
-                if (index < 0) return;
-                if (index >= $wrapper.children().length) return;
+                var length = $wrapper.children().length;
+                index = (index % length + length) % length;
 
                 $items.scrollLeft(0);
                 $wrapper.css('transform', 'perspective(1000px) translateX(' + index * -100 + '%)');
@@ -70,12 +95,6 @@
 
             $next.on('click', function () {
                 move(now + 1);
-            });
-
-            // Banner Link Focus
-            $wrapper.on('focus', 'a', function () {
-                var index = $(this).index();
-                move(index);
             });
         })();
 
